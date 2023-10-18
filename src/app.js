54fb1eec97f98427f68fb80621a5911d9f7b89b9
michel sampil <michel.sampil@gmail.com>
2023-10-18 20:34:46 -0300

    cleaning app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import express from "express";
+import errorHandler from "./middlewares/error-handler.mjs";
+import { requestLogger } from "./middlewares/request-logger.mjs";
+import tasksRouter from "./routes/tasks.mjs";
+
 const app = express();
 const port = 3000;
-
-import { requestLogger } from "./middlewares/request-logger.mjs";
-import errorHandler from "./middlewares/error-handler.mjs";
 
 app.use(express.json());
 
 app.use(requestLogger);
 
-import tasksRouter from "./routes/tasks.mjs";
 app.use("/api", tasksRouter);
 
 app.use(errorHandler);
